316aaf848c6702a0dae4ab700ee0593f0b844c27
Bradford Holden <brad.p.holden@gmail.com>
2016-09-08 11:32:15 -0700

    options added
    optparse
    added output and input directory
    made default data file an option

--- makevels.py
+++ makevels.py
@@ -4,13 +4,27 @@ from Generate_Velocities import readin_data
 from Generate_Velocities import calc_rvs
 from Generate_Velocities import write_vels
 from Generate_Velocities import write_sys
+import optparse
+import os
 
+parser = optparse.OptionParser()
+parser.add_option("-f","--file",dest="datafile",default="../Datafiles/TESSAPF_AWMasses_prec_phase.csv")
+parser.add_option("-i","--indir",dest="indir",default="../SimFiles/")        
+parser.add_option("-o","--outdir",dest="outdir",default=".")        
+(options, args) = parser.parse_args()    
+
+
+if not os.path.isdir(options.outdir):
+    os.mkdir(options.outdir)
+ 
 #Read in TESSAPF master data file
-file='../Datafiles/TESSAPF_AWMasses_prec_phase.csv'
-TESSAPFdata=ascii.read(file,format='csv')
+
+TESSAPFdata=ascii.read(options.datafile,format='csv')
 
 #Make a list of all of the APF simulation files we have
-whole_filelist=glob.glob('../SimFiles/*.sim')
+if options.indir[-1] != "/":
+    options.indir =  options.indir + "/"
+whole_filelist=glob.glob(options.indir + '*.sim')
 
 #Determine which stars are TESSAPF Stars and need .vels and .sys files
 sub='TESSAPF'
@@ -32,7 +46,7 @@ for i in range(0,len(filelist)):
     velocities,phases=calc_rvs(TESSAPFdata,simdata,starname)
     
     #write .vels file for each star
-    write_vels(starname,simdata,velocities,phases)
+    write_vels(starname,simdata,velocities,phases,outdir=options.outdir)
 
     #write .sys file for each star
-    write_sys(TESSAPFdata,starname)
+    write_sys(TESSAPFdata,starname,outdir=options.outdir)
