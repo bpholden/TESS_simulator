ca9031831a36e9649fc14fb77260a8d80e50d18d
Bradford Holden <brad.p.holden@gmail.com>
2017-11-21 16:39:49 -0800

    typo

--- consts.py
+++ consts.py
@@ -1,5 +1,5 @@
 import scipy.constants as sc
 
 JD0=2458362.5  #(Sept 1, 2018 - 1 year after TESS launches)
-ECC=0.              #TESS similation sets all ecc=0
+ECC=0.              #TESS simulation sets all ecc=0
 OMEGA=-sc.pi/2
