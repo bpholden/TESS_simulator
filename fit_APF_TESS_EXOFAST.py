5026e40157c104d581b6ed11cbf05140d25aa7b8
Bradford Holden <brad.p.holden@gmail.com>
2018-01-20 13:33:20 -0800

    make exofast wrapper actually work

--- fit_APF_TESS_EXOFAST.py
+++ fit_APF_TESS_EXOFAST.py
@@ -66,156 +66,63 @@ def parse_options():
 
     return snames,options.infile,options.mfn,veldir,options.outdir,options.extra
 
-def initialize_model(planets,TESSAPFdata,addextra=False):
+def initialize_model(sname,planets,TESSAPFdata,addextra=False):
     time_base = 2458362.5
+
+    priorname = sname + ".priors"
+    priorfp = open(priorname,"w+")
+    fitname = "fit_" + sname + ".pro"
+    fitfp = open(fitname,"w+")
+
+    fitfp.write("pro fit_" + sname + ",debug=debug\n")
+    fitfp.write("\nmaxsteps=100000\nnthin=2\n")
 
     nplan = len(planets)
     if addextra:
         nplan = nplan + 1
-    params = radvel.Parameters(nplan,basis='per tp e w k') # number of planets = nplan
-    n = 1
+
+    rvpath = "%s.vels" % (sname)
+    execstr = "exofastv2, nplanets=%d,rvpath='%s',priorfile='%s',prefix='%s'," % (nplan,rvpath,priorname,sname)
+    execstr = execstr + "maxsteps=maxsteps,nthin=nthin,"
+    trues = ""
+    falses = ""
+    for i in range(0,nplan):
+        trues += "1,"
+        falses +=  "0,"
+    trues = trues.rstrip(",")
+    falses = falses.rstrip(",")
+    execstr = execstr + "circular=[%s],fitrv=[%s]" % (trues,trues)
+    execstr = execstr + ",debug=debug"
+    fitfp.write(execstr + "\n")
+    fitfp.write("\nend\n")
+    fitfp.close()
+    n = 0
+    ks = Generate_Velocities.calc_K(TESSAPFdata,sname)
+    
+    priorfp.write("mstar %f 0\n" % (TESSAPFdata['mstar'][planets[0]]))  
     for idx in planets:
         c_period = TESSAPFdata['period'][idx]
-        k = radvel.utils.semi_amplitude(TESSAPFdata['est_mass'][idx],c_period,TESSAPFdata['mstar'][idx],0.)
+        priorfp.write("period_%d %f 0\n" % (n,c_period))
+        priorfp.write("k_%d %f 15.\n" % (n,float(ks[n])))
         tpstart = time_base + c_period * (TESSAPFdata['phase'][idx] + 0.25)
-        params['per%d' % (n)] = radvel.Parameter(value=c_period)
-        params['tp%d' % (n)] = radvel.Parameter(value=tpstart)
-        params['e%d' % (n)] = radvel.Parameter(value=0.0)
-        params['w%d' % (n)] = radvel.Parameter(value=0.)
-        params['k%d' % (n)] = radvel.Parameter(value=k)
+        priorfp.write("tc_%d %f 15.\n" % (n,tpstart))
+#        priorfp.write("cosi_%d 0. 0." % (idx))                         
         n = n + 1
 
     if addextra:
-        params['per%d' % (n)] = radvel.Parameter(value=30.)
-        params['tp%d' % (n)] = radvel.Parameter(value=time_base + 15.)
-        params['e%d' % (n)] = radvel.Parameter(value=0.0001)
-        params['w%d' % (n)] = radvel.Parameter(value=np.pi/2.)
-        params['k%d' % (n)] = radvel.Parameter(value=2.)
+
+        priorfp.write("period_%d 30 15\n" % (n))
+        priorfp.write("k_%d 2.\n" % (n))
+        tpstart = time_base + 15.
+        priorfp.write("tc_%d %f\n" % (n,tpstart))
+        priorfp.write("cosi_%d 0. 0.\n" % (idx))                         
         
-#     nparams = params.basis.from_cps(params,'per tc e w k') # transform to new basis (for less biased fits?)
-#     n = 1
-#     if addextra:
-#         n = 2
-#     for i in range(1,len(planets+n)):
-# #         del nparams['e%d' % (i)]
-# # #        del nparams['k%d' % (i)]
-# #         del nparams['w%d' % (i)]
-#         del nparams['tp%d' % (i)]
-#     # new basis
-    
-    mod = radvel.RVModel(params, time_base=time_base)
-    mod.params['dvdt'] = radvel.Parameter(value=0.0)
-    mod.params['curv'] = radvel.Parameter(value=0.0)
-    return mod
+    priorfp.close()
 
+    execstr = "idl -e '%s'" %(fitname)
 
-def make_like(mod,invels,planets,addextra=False):
-    like = radvel.likelihood.RVLikelihood(mod, invels['time'], invels['mnvel'], invels['errvel'])
-    gamma_guess = (invels['mnvel'].max() + invels['mnvel'].min())/2.
-    
-    like.params['gamma'] = radvel.Parameter(value=0.01)
-    like.params['jit']= radvel.Parameter(value=3.)
-    np = like.params.num_planets
-    if addextra:
-        np = np+1
-    for n in range(1,np+1):
-        like.params['w%d' % (n)].vary = True
-        like.params['e%d'% (n)].vary = False
-        like.params['per%d' % (n)].vary = False
-        like.params['tp%d' % (n)].vary = False
-#        like.params['secosw%d' % (n)].vary = False
-#        like.params['sesinw%d'% (n)].vary = False
-#        like.params['per%d' % (n)].vary = False
-#        like.params['tc%d' % (n)].vary = True
+    return execstr
 
-
-        
-    like.params['curv'].vary = False
-    like.params['jit'].vary = True
-    like.params['gamma'].vary = True        
-    like.params['dvdt'].vary = False    
-    
-    return like
-
-def init_posterior(like,planets,addextra=False):
-
-    post = radvel.posterior.Posterior(like)
-    post.priors += [radvel.prior.Gaussian( 'jit', 3, 3)]
-    post.priors += [radvel.prior.Gaussian( 'gamma', float(like.params['gamma'].value), 30)]
-    n = 1
-    ntot = len(planets)
-    if addextra:
-        ntot = ntot + 1
-        
-#    for idx in range(0,ntot):
-#        n = idx + 1
-#        post.priors += [radvel.prior.Gaussian( 'k%d' % (n), np.log(5), 10)]
-    post.priors += [radvel.prior.PositiveKPrior(ntot)]
-    return post
-
-def plot_results(post,chains,outdir,sname):
-
-    radvel.plotting.corner_plot(post, chains,saveplot=os.path.join(outdir,sname+"_corner_cps_efix.pdf"))
-    radvel.plotting.rv_multipanel_plot(post,saveplot=os.path.join(outdir,sname+"_mp_cps_efix.pdf"))
-
-    return
-
-def writefit(sname,outdir,periods,Ks,err_Ks,Ms,err_Ms,Kmls,rplanets,nvels):
-
-    outname = "%s_efix.radvelfit" % (sname)
-    outname = os.path.join(outdir,outname)
-    outfp = open(outname,"w")
-    for i in range(0,len(Ks)):
-        ostr = "%f %f %f %f %f %f %d %f\n" % (periods[i],Ks[i],err_Ks[i],Ms[i],err_Ms[i],rplanets[i],nvels,Kmls[i])
-        # period (days) K (m/s) err_K (m/s) planet mass (M_earth) error (M_earth) R (R_earth) #vs 
-        outfp.write(ostr)
-
-    outfp.close()
-
-def mcmc_planets(post,outdir,sname,mstars,addextra=False):
-    
-    conf_base = sname
-    nwalkers = 50
-    nsteps = 10000
-    ensembles = 8
-    msg = "Running MCMC for {}, N_ensembles = {}, N_walkers = {}, N_steps = {} ...".format(
-        conf_base, ensembles, nwalkers, nsteps)
-    print msg
-
-    chains = radvel.mcmc(
-        post, nwalkers=nwalkers, nrun=nsteps,
-        ensembles=ensembles
-    )
-
-    Ks = []
-    err_Ks = []
-    Ms = []
-    err_Ms = []
-    # Get quantiles and update posterior object
-    post_summary=chains.quantile([0.159, 0.5, 0.841])
-    c = 1
-    if addextra:
-        c = 2
-    for n in range(1,post.params.num_planets+c):
-        post_summary['Mpsini%d' % (n)] = radvel.utils.Msini((post_summary['k%d' % (n)]),post.params['per%d' % (n)].value,mstars[0],0.)
-        if writefit:
-#            Ks.append(post.params['k%d' % (n)].value)
-            Ks.append(post_summary['k%d' % (n)][0.5])
-            sigK = ( post_summary['k%d' % (n)][0.841] - post_summary['k%d' % (n)][0.159] ) / 2.
-            sigK = sigK #* np.exp(post_summary['logk%d' % (n)][0.5])
-            err_Ks.append(sigK)
-#            Ms.append(radvel.utils.Msini((post.params['k%d' % (n)]).value,post.params['per%d' % (n)].value,mstars[0],0.))
-            Ms.append(post_summary['Mpsini%d' % (n)][0.5])
-            err_Ms.append(( post_summary['Mpsini%d' % (n)][0.841] - post_summary['Mpsini%d' % (n)][0.159]) / 2.)
-
-
-
-
-    print "Saving output files..."
-    saveto = os.path.join(outdir, sname+'_post_summary_cps_efix_wfix.csv')
-    post_summary.to_csv(saveto, sep=',')
-    
-    return chains,Ks,err_Ks,Ms,err_Ms
 
 if __name__ == "__main__":
 
@@ -231,26 +138,6 @@ if __name__ == "__main__":
             
     for sname in snames:
 
-        sfn = sname + ".sys"
-        vfn = sname + ".vels"
-        newvels = True
-
-        invels = readin_velsfile(os.path.join(veldir,vfn))
-#        ddates,dphases, dvels, derrs, di2sums = bin_phase_dates(invels["time"],invels["pphase"],invels['mnvel'],invels["errvel"],invels["i2val"])
-#        bvfn = sname + "binned"
-#        ascii.write([ddates,dvels,derrs,di2sums,dphases], os.path.join(veldir,bvfn+".vels"),format="no_header")
-            
-        if len(invels) > 4 :
-            planets, = np.where((TESSAPFdata['star_names'] == sname) & (TESSAPFdata['detected'] == "TRUE"))
-
-            mod = initialize_model(planets,TESSAPFdata,addextra=addextra)
-            like = make_like(mod,invels,planets,addextra=addextra)
-            post = init_posterior(like,planets,addextra=addextra)
-            post = radvel.fitting.maxlike_fitting(post, verbose=False)
-            print post
-            for i in range(0,len(planets)):
-                Kmls.append(post.params['k%d' % (i+1)].value)
-            chains,Ks,err_Ks,Ms,err_Ms = mcmc_planets(post,outdir,sname,TESSAPFdata['mstar'][planets],addextra=addextra)
-#            write_output(post,TESSAPFdata['phase'][planets],TESSAPFdata['vmag'][planets],TESSAPFdata['rplanet'][planets],TESSAPFdata['true_mass'][planets],TESSAPFdata['Index'][planets],gd['vel_offset'][gd['starname'] == sname],writefit=True,veldir=veldir,outdir=outdir)
-            plot_results(post,chains,outdir,sname)
-            writefit(sname,outdir,TESSAPFdata['period'][planets],Ks,err_Ks,Ms,err_Ms,Kmls,TESSAPFdata['rplanet'][planets],len(invels))
+        planets, = np.where((TESSAPFdata['star_names'] == sname) & (TESSAPFdata['detected'] == "TRUE"))
+        estr = initialize_model(sname,planets,TESSAPFdata,addextra=addextra)
+        print ( estr )
