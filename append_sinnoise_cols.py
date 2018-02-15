8b78da4374e04bc3228add3c3ac5c5c273a224b0
Bradford Holden <brad.p.holden@gmail.com>
2018-02-15 15:15:14 -0800

    update to reflect new columns and new long period

--- append_sinnoise_cols.py
+++ append_sinnoise_cols.py
@@ -2,16 +2,20 @@ import numpy as np
 from astropy.io import ascii
 from astropy.table import Table, Column
 
-fn = "newgoogledex.csv"
+fn = "newgoogledex_R1.csv"
 ofn = "newgoogledex_sinnoise.csv"
 
 gd = ascii.read(fn)
-gd['longphase']=np.random.uniform(size=len(gd['vmag']))
 gd['shortphase']=np.random.uniform(size=len(gd['vmag']))
 start = 2.5 / (24.*60.)
 end = 12.5 / (24.*60.)
 gd['shortperiod']=np.random.uniform(start,end,size=len(gd['vmag']))
-start = 15.
-end = 45.
-gd['longperiod']=np.random.uniform(start,end,size=len(gd['vmag']))
+
+
+gd['longphase']=np.random.uniform(size=len(gd['vmag']))
+gd['longperiod']=gd['rot_period']
+
+gd['pri_offset']=np.zeros_like(gd['vmag'])
+gd['vel_offset']=np.zeros_like(gd['vmag'])
+
 ascii.write(gd, output=ofn, format="csv")
